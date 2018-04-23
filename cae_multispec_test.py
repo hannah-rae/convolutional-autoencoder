061a9c5590bfedd1b49c841d6a4c72a56f1f4620
hannah-rae <hkerner@asu.edu>
2018-04-23 11:52:23 -0700

    Modified models for multispectral images

--- cae_multispec_test.py
+++ cae_multispec_test.py
@@ -39,7 +39,9 @@ def autoencoder(input_shape,
 
     # input to the network
     x = tf.placeholder(tf.float32, input_shape, name='x')
-    # fraction of neurons to keep in dropout layer
+    # Use this to randomly crop images on the fly, rather than holding everything in memory
+    #x = tf.map_fn(lambda frame: tf.random_crop(value=frame, size=[64,64,6]), x)
+
     keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
     # %%
@@ -59,9 +61,6 @@ def autoencoder(input_shape,
 
     # Linearly scale image to have zero mean and unit norm
     current_input = tf.map_fn(lambda frame: tf.image.per_image_standardization(frame), current_input)
-    # Randomly fluctuate the brightness to help with overfitting
-    #current_input = tf.map_fn(lambda frame: tf.image.random_brightness(frame, max_delta=32. / 255.), current_input)
-    input_image = current_input
 
     # %%
     # Build the encoder
@@ -87,8 +86,8 @@ def autoencoder(input_shape,
         output = lrelu(
             tf.add(tf.nn.conv2d(
                 current_input, W, strides=[1, stride, stride, 1], padding='SAME'), b))
-        #output_drop = tf.nn.dropout(x=output, keep_prob=keep_prob, name='conv_dropout')
-        current_input = output
+        output_drop = tf.nn.dropout(x=output, keep_prob=keep_prob, name='conv_dropout')
+        current_input = output_drop
 
     # %%
     # store the latent representation
@@ -104,10 +103,6 @@ def autoencoder(input_shape,
             stride=1
         else:
             stride=2
-        # W = tf.Variable(
-        #     tf.random_uniform(encoder[layer_i].get_shape().as_list(),
-        #         -1.0 / math.sqrt(encoder[layer_i].get_shape().as_list()[2]),
-        #         1.0 / math.sqrt(encoder[layer_i].get_shape().as_list()[2])))
         W = encoder[layer_i] # should be clipped already
         b = tf.Variable(tf.zeros([W.get_shape().as_list()[2]]))
         output = lrelu(tf.add(
@@ -126,9 +121,8 @@ def autoencoder(input_shape,
     # %%
     # now have the reconstruction through the network
     y = current_input
-
+    # cost function measures pixel-wise difference
     cost = tf.losses.mean_squared_error(x_tensor, y)
-    
 
     # %%
     return {'x': x, 'z': z, 'y': y, 'cost': cost, 'keep_prob': keep_prob}
@@ -140,11 +134,6 @@ def test_multispec():
     from os import mkdir
     import cv2
 
-    # load mastcam data
-    # mastcam = dataset.load_mcam_6f()
-    # print mastcam.shape
-    # mean_img = np.mean(mastcam, axis=0)
-    # std_img = np.std(mastcam, axis=0)
     ae = autoencoder(input_shape=[None, 64, 64, 6])
 
     learning_rate = 0.01
@@ -152,40 +141,17 @@ def test_multispec():
 
     # We create a session to use the graph
     sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
 
     # Add ops to save and restore all the variables.
     saver = tf.train.Saver()
-
-    # Fit all training data
-    n_epochs = 15
-    batch_size = 100
-    num_batches = dataset.num_train_ex / batch_size
-    print "num batches = %d", num_batches
-    for epoch_i in range(n_epochs):
-        for batch_i in range(num_batches):
-            # idx = batch_i*batch_size
-            # batch_xs = mastcam[idx:idx+batch_size]
-            batch_xs = dataset.next_batch_6f(batchsize=batch_size)
-            #train = np.array([img - mean_img for img in batch_xs])
-            sess.run(optimizer, feed_dict={ae['x']: batch_xs, ae['keep_prob']: 0.6})
-            #print(batch_i, sess.run(ae['cost'], feed_dict={ae['x']: batch_xs, ae['keep_prob']: 0.7}))
-        print(epoch_i, sess.run(ae['cost'], feed_dict={ae['x']: batch_xs, ae['keep_prob']: 1.0}))
+    saver.restore(sess, '/home/hannah/src/MastcamCAE/saved_sessions/12-8-3_7-5-3_nodrop_epochs15/1524259856.ckpt')
     
     # Save the model for future training or testing
     t = str(int(time()))
-    save_path = saver.save(sess, "/home/hannah/src/MastcamCAE/saved_sessions/%s.ckpt" % t)
-    #save_path = saver.save(sess, "/scratch/hannah/MastcamCAE/saved_sessions/%s.ckpt" % t)
-    print("Model saved in path: %s" % save_path)
-
 
     # %%
     # Plot example reconstructions and record reconstruction errors
-    #n_examples = 10
-    test_xs, x_names = dataset.load_mcam_DW_test()
-    #test_xs_norm = np.array([img - mean_img for img in test_xs])
-    #recon, err = sess.run([ae['y'], ae['cost']], feed_dict={ae['x']: test_xs, ae['keep_prob']: 1.0})
-    #recon = np.array([img + mean_img for img in recon])
+    test_xs, x_names = dataset.load_mcam_6f_train()
 
     mkdir('./results/' + t)
 
--- dataset.py
+++ dataset.py
@@ -3,176 +3,287 @@ import numpy as np
 from glob import glob
 from random import shuffle
 
-def load_test_gray2rgb():
-    test_dir = '/home/hannah/data/mcam_vae/cdf_test_gray'
-    num_images = len(glob(test_dir + '/*'))
-    # Create an empty array to hold all the downsampled images
-    dataset = np.zeros((num_images, 144, 160, 3))
-    for i, img_fn in enumerate(glob(test_dir + '/*')):
-        image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
-        dataset[i] = image
-    print dataset.shape
-    np.random.shuffle(dataset)
-    return dataset
+# def load_test_gray2rgb():
+#     test_dir = '/home/hannah/data/mcam_vae/cdf_test_gray'
+#     num_images = len(glob(test_dir + '/*'))
+#     # Create an empty array to hold all the downsampled images
+#     dataset = np.zeros((num_images, 144, 160, 3))
+#     for i, img_fn in enumerate(glob(test_dir + '/*')):
+#         image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
+#         dataset[i] = image
+#     print dataset.shape
+#     np.random.shuffle(dataset)
+#     return dataset
 
-def load_test_earth():
-    test_dir = '/home/hannah/data/mcam_vae/earth'
-    num_images = len(glob(test_dir + '/*'))
-    # Create an empty array to hold all the downsampled images
-    dataset = np.zeros((num_images, 144, 160, 3))
-    for i, img_fn in enumerate(glob(test_dir + '/*')):
-        image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
-        image = image[:144, :160, :]
-        dataset[i] = image
-    print dataset.shape
-    np.random.shuffle(dataset)
-    return dataset
+# def load_test_earth():
+#     test_dir = '/home/hannah/data/mcam_vae/earth'
+#     num_images = len(glob(test_dir + '/*'))
+#     # Create an empty array to hold all the downsampled images
+#     dataset = np.zeros((num_images, 144, 160, 3))
+#     for i, img_fn in enumerate(glob(test_dir + '/*')):
+#         image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
+#         image = image[:144, :160, :]
+#         dataset[i] = image
+#     print dataset.shape
+#     np.random.shuffle(dataset)
+#     return dataset
 
-def load_test_pups():
-    test_dir = '/home/hannah/data/mcam_vae/puppies'
-    num_images = len(glob(test_dir + '/*'))
-    # Create an empty array to hold all the downsampled images
-    dataset = np.zeros((num_images, 144, 160, 3))
-    for i, img_fn in enumerate(glob(test_dir + '/*')):
-        image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
-        image = image[7:151, 7:167, :]
-        dataset[i] = image
-    print dataset.shape
-    np.random.shuffle(dataset)
-    return dataset
+# def load_test_pups():
+#     test_dir = '/home/hannah/data/mcam_vae/puppies'
+#     num_images = len(glob(test_dir + '/*'))
+#     # Create an empty array to hold all the downsampled images
+#     dataset = np.zeros((num_images, 144, 160, 3))
+#     for i, img_fn in enumerate(glob(test_dir + '/*')):
+#         image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
+#         image = image[7:151, 7:167, :]
+#         dataset[i] = image
+#     print dataset.shape
+#     np.random.shuffle(dataset)
+#     return dataset
 
-def load_mcam_gray():
-    train_dir = '/home/hannah/data/mcam_vae/train'
-    num_images = len(glob(train_dir + '/*'))
-    # Create an empty array to hold all the downsampled images
-    dataset = np.zeros((num_images, 144*144))
-    for i, img_fn in enumerate(glob(train_dir + '/*')):
-        image = cv2.imread(img_fn, cv2.IMREAD_GRAYSCALE)
-        image = image[:, 7:151]
-        dataset[i] = image.flatten()
-    print dataset.shape
-    np.random.shuffle(dataset)
-    return dataset
+# def load_mcam_gray():
+#     train_dir = '/home/hannah/data/mcam_vae/train'
+#     num_images = len(glob(train_dir + '/*'))
+#     # Create an empty array to hold all the downsampled images
+#     dataset = np.zeros((num_images, 144*144))
+#     for i, img_fn in enumerate(glob(train_dir + '/*')):
+#         image = cv2.imread(img_fn, cv2.IMREAD_GRAYSCALE)
+#         image = image[:, 7:151]
+#         dataset[i] = image.flatten()
+#     print dataset.shape
+#     np.random.shuffle(dataset)
+#     return dataset
 
-def test_mcam_slices(i_len=36, j_len=40):
-    n = 16
-    def get_slices(image):
-        slices = np.zeros((n, i_len, j_len, 3))
-        idx = 0
-        for i in range(4):
-            for j in range(4):
-                sl = image[i*i_len:i*i_len+i_len, j*j_len:j*j_len+j_len]
-                slices[idx] = sl
-                idx = idx + 1
-        return slices
+# def test_mcam_slices(i_len=36, j_len=40):
+#     n = 16
+#     def get_slices(image):
+#         slices = np.zeros((n, i_len, j_len, 3))
+#         idx = 0
+#         for i in range(4):
+#             for j in range(4):
+#                 sl = image[i*i_len:i*i_len+i_len, j*j_len:j*j_len+j_len]
+#                 slices[idx] = sl
+#                 idx = idx + 1
+#         return slices
 
-    test_dir = '/home/hannah/data/mcam_vae/cdf_test'
-    num_images = len(glob(test_dir + '/*'))
-    # Create an empty array to hold all the downsampled images
-    dataset = np.zeros((num_images*n, i_len, j_len, 3))
-    for i, img_fn in enumerate(glob(test_dir + '/*')):
-        image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
-        image_slices = get_slices(image)
-        dataset[i*n:i*n+n] = image_slices
-    print dataset.shape
-    np.random.shuffle(dataset)
-    return dataset
+#     test_dir = '/home/hannah/data/mcam_vae/cdf_test'
+#     num_images = len(glob(test_dir + '/*'))
+#     # Create an empty array to hold all the downsampled images
+#     dataset = np.zeros((num_images*n, i_len, j_len, 3))
+#     for i, img_fn in enumerate(glob(test_dir + '/*')):
+#         image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
+#         image_slices = get_slices(image)
+#         dataset[i*n:i*n+n] = image_slices
+#     print dataset.shape
+#     np.random.shuffle(dataset)
+#     return dataset
 
-def load_mcam_slices(i_len=36, j_len=40):
-    n = 16
-    def get_slices(image):
-        slices = np.zeros((n, i_len, j_len, 3))
-        idx = 0
-        for i in range(4):
-            for j in range(4):
-                sl = image[i*i_len:i*i_len+i_len, j*j_len:j*j_len+j_len]
-                slices[idx] = sl
-                idx = idx + 1
-        return slices
+# def load_mcam_slices(i_len=36, j_len=40):
+#     n = 16
+#     def get_slices(image):
+#         slices = np.zeros((n, i_len, j_len, 3))
+#         idx = 0
+#         for i in range(4):
+#             for j in range(4):
+#                 sl = image[i*i_len:i*i_len+i_len, j*j_len:j*j_len+j_len]
+#                 slices[idx] = sl
+#                 idx = idx + 1
+#         return slices
 
-    train_dir = '/home/hannah/data/mcam_vae/train'
-    num_images = len(glob(train_dir + '/*'))
-    # Create an empty array to hold all the downsampled images
-    dataset = np.zeros((num_images*n, i_len, j_len, 3))
-    for i, img_fn in enumerate(glob(train_dir + '/*')):
-        image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
-        image_slices = get_slices(image)
-        dataset[i*n:i*n+n] = image_slices
-    print dataset.shape
-    np.random.shuffle(dataset)
-    return dataset
+#     train_dir = '/home/hannah/data/mcam_vae/train'
+#     num_images = len(glob(train_dir + '/*'))
+#     # Create an empty array to hold all the downsampled images
+#     dataset = np.zeros((num_images*n, i_len, j_len, 3))
+#     for i, img_fn in enumerate(glob(train_dir + '/*')):
+#         image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
+#         image_slices = get_slices(image)
+#         dataset[i*n:i*n+n] = image_slices
+#     print dataset.shape
+#     np.random.shuffle(dataset)
+#     return dataset
 
-def load_test_gray():
-    test_dir = '/home/hannah/data/mcam_vae/cdf_test'
-    num_images = len(glob(test_dir + '/*'))
-    # Create an empty array to hold all the downsampled images
-    dataset = np.zeros((num_images, 144*144))
-    for i, img_fn in enumerate(glob(test_dir + '/*')):
-        image = cv2.imread(img_fn, cv2.IMREAD_GRAYSCALE)
-        image = image[:, 7:151]
-        dataset[i] = image.flatten()
-    print dataset.shape
-    np.random.shuffle(dataset)
-    return dataset
+# def load_test_gray():
+#     test_dir = '/home/hannah/data/mcam_vae/cdf_test'
+#     num_images = len(glob(test_dir + '/*'))
+#     # Create an empty array to hold all the downsampled images
+#     dataset = np.zeros((num_images, 144*144))
+#     for i, img_fn in enumerate(glob(test_dir + '/*')):
+#         image = cv2.imread(img_fn, cv2.IMREAD_GRAYSCALE)
+#         image = image[:, 7:151]
+#         dataset[i] = image.flatten()
+#     print dataset.shape
+#     np.random.shuffle(dataset)
+#     return dataset
 
-def load_mcam_rgb():
-    train_dir = '/home/hannah/data/mcam_vae/train'
-    num_images = len(glob(train_dir + '/*'))
-    # Create an empty array to hold all the downsampled images
-    dataset = np.zeros((num_images, 144, 160, 3))
-    for i, img_fn in enumerate(glob(train_dir + '/*')):
-        image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
-        # im_red = image[:,:,2]
-        # im_blue = image[:,:,0]
-        # image[:,:,0] = im_red
-        # image[:,:,2] = im_blue
-        # dataset[i,:,:,:] = image
-        dataset[i] = image
-        #dataset[i] = image.flatten()
-    print dataset.shape
-    np.random.shuffle(dataset)
-    return dataset
+# def load_mcam_rgb():
+#     train_dir = '/home/hannah/data/mcam_vae/train'
+#     num_images = len(glob(train_dir + '/*'))
+#     # Create an empty array to hold all the downsampled images
+#     dataset = np.zeros((num_images, 144, 160, 3))
+#     for i, img_fn in enumerate(glob(train_dir + '/*')):
+#         image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
+#         # im_red = image[:,:,2]
+#         # im_blue = image[:,:,0]
+#         # image[:,:,0] = im_red
+#         # image[:,:,2] = im_blue
+#         # dataset[i,:,:,:] = image
+#         dataset[i] = image
+#         #dataset[i] = image.flatten()
+#     print dataset.shape
+#     np.random.shuffle(dataset)
+#     return dataset
 
-def load_mcam_6f():
+train_dir = '/home/hannah/data/mcamrdrs/train/cropped'
+train_examples = glob(train_dir + '/*.npy')
+np.random.shuffle(train_examples)
+num_train_ex = len(train_examples)
+# TODO: Make epoch size a flag. Currently this is hardcoded for convenience.
+train_examples = train_examples*15
+
+def next_batch_6f(batchsize=5):
+    # Each image is stored a 64 x 64 x 6 numpy array
+    # Create an empy array to hold the batch of numpy arrays
+    batch = np.zeros((batchsize, 64, 64, 6))
+    # Store the images in the batch array
+    for i in range(batchsize):
+        ex = train_examples.pop()
+        batch[i] = np.load(ex)
+
+    #print 'this batch ', batch.shape
+    #print 'train examples remaining ', len(train_examples)
+    return batch
+
+def load_mcam_6f_train():
     train_dir = '/home/hannah/data/mcam_Lall_Rall_64x64/train'
-    num_images = len(glob(train_dir + '/*')[:20000])
+    hw_dir = '/home/hannah/data/mcam_Lall_Rall_64x64/holdout_hardware'
+    train_names = glob(train_dir + '/*')
+    train_names = train_names + glob(hw_dir + '/*')
+    # np.random.shuffle(train_names)
+    # train_names = train_names[:5000]
+    num_images = len(train_names)
     # Each image is stored a 64 x 64 x 6 numpy array
     # Create an empy array to hold all the numpy arrays
     dataset = np.zeros((num_images, 64, 64, 6))
     # Store the images in the dataset array
-    for i, img_fn in enumerate(glob(train_dir + '/*')[:20000]):
-        dataset[i] = np.load(img_fn)
-    print dataset.shape
-    np.random.shuffle(dataset)
-    return dataset
-
-def load_mcam_6f_test():
-    test_dir = '/home/hannah/data/mcam_Lall_Rall_64x64/test'
-    num_images = len(glob(test_dir + '/*')[:100])
-    # Each image is stored a 64 x 64 x 6 numpy array
-    # Create an empy array to hold all the numpy arrays
-    dataset = np.zeros((num_images, 64, 64, 6))
-    # Store the images in the dataset array
-    for i, img_fn in enumerate(glob(test_dir + '/*')[:100]):
+    for i, img_fn in enumerate(train_names):
         dataset[i] = np.load(img_fn)
     print dataset.shape
     #np.random.shuffle(dataset)
-    return dataset
+    return dataset, [name.split('/')[-1][:-4] for name in train_names]
 
-def load_test_rgb():
-    test_dir = '/home/hannah/data/mcam_vae/cdf_test'
-    num_images = len(glob(test_dir + '/*'))
-    # Create an empty array to hold all the downsampled images
-    dataset = np.zeros((num_images, 144, 160, 3))
-    for i, img_fn in enumerate(glob(test_dir + '/*')):
-        image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
-        # im_red = image[:,:,2]
-        # im_blue = image[:,:,0]
-        # image[:,:,0] = im_red
-        # image[:,:,2] = im_blue
-        # dataset[i,:,:,:] = image
-        dataset[i] = image
-        #dataset[i] = image.flatten()
+def load_mcam_6f_test():
+    test_dir = '/home/hannah/data/mcam_Lall_Rall_64x64/test'
+    test_names = glob(test_dir + '/*')
+    num_images = len(test_names)
+    # Each image is stored a 64 x 64 x 6 numpy array
+    # Create an empy array to hold all the numpy arrays
+    dataset = np.zeros((num_images, 64, 64, 6))
+    # Store the images in the dataset array
+    for i, img_fn in enumerate(test_names):
+        dataset[i] = np.load(img_fn)
+    print dataset.shape
+    #np.random.shuffle(dataset)
+    return dataset#, [name.split('/')[-1][:-4] for name in test_names]
+
+def load_mcam_DW_test_subject(subject):
+    test_dir = '/home/hannah/data/mcam_multispec_DW_subject/' + subject
+    test_names = glob(test_dir + '/*.npy')
+    num_images = len(test_names)
+    # Each image is stored a 64 x 64 x 6 numpy array
+    # Create an empy array to hold all the numpy arrays
+    dataset = np.zeros((num_images, 64, 64, 6))
+    # Store the images in the dataset array
+    for i, img_fn in enumerate(test_names):
+        dataset[i] = np.load(img_fn)
+    print dataset.shape
+    #np.random.shuffle(dataset)
+    return dataset, [name.split('/')[-1][:-4] for name in test_names]
+
+def load_mcam_DW_test_MR():
+    test_dir = '/home/hannah/data/mcam_multispec_DW'
+    test_names = glob(test_dir + '/*L*.npy')
+    num_images = len(test_names)
+    # Each image is stored a 64 x 64 x 6 numpy array
+    # Create an empy array to hold all the numpy arrays
+    dataset = np.zeros((num_images, 64, 64, 6))
+    # Store the images in the dataset array
+    for i, img_fn in enumerate(test_names):
+        dataset[i] = np.load(img_fn)
+    print dataset.shape
+    #np.random.shuffle(dataset)
+    return dataset, [name.split('/')[-1][:-4] for name in test_names]
+
+def load_mcam_DW_test_ML():
+    test_dir = '/home/hannah/data/mcam_multispec_DW'
+    test_names = glob(test_dir + '/*R*.npy')
+    num_images = len(test_names)
+    # Each image is stored a 64 x 64 x 6 numpy array
+    # Create an empy array to hold all the numpy arrays
+    dataset = np.zeros((num_images, 64, 64, 6))
+    # Store the images in the dataset array
+    for i, img_fn in enumerate(test_names):
+        dataset[i] = np.load(img_fn)
+    print dataset.shape
+    #np.random.shuffle(dataset)
+    return dataset, [name.split('/')[-1][:-4] for name in test_names]
+
+def load_mcam_DW_test():
+    #test_dir = '/home/hannah/data/mcam_multispec_DW'
+    test_dir = '/home/hannah/data/mcamrdrs/expert/cropped'
+    test_names = glob(test_dir + '/*.npy')
+    num_images = len(test_names)
+    # Each image is stored a 64 x 64 x 6 numpy array
+    # Create an empy array to hold all the numpy arrays
+    dataset = np.zeros((num_images, 64, 64, 6))
+    # Store the images in the dataset array
+    for i, img_fn in enumerate(test_names):
+        dataset[i] = np.load(img_fn)
+    print dataset.shape
+    #np.random.shuffle(dataset)
+    return dataset, [name.split('/')[-1][:-4] for name in test_names]
+
+def load_explanation_images(path_to_results):
+    image_dirs = glob(path_to_results+'/*')
+    images = [glob(d+'/*explanation*')[0] for d in image_dirs] # N x 64 x 64 array
+    num_images = len(images)
+    # Each image is stored a 64 x 64 numpy array
+    # Create an empy array to hold all the numpy arrays
+    dataset = np.zeros((num_images, 64, 64), dtype=np.float32)
+    # Store the images in the dataset array
+    for i, img_fn in enumerate(images):
+        dataset[i] = np.load(img_fn).astype(np.float32)
     print dataset.shape
     np.random.shuffle(dataset)
     return dataset
+
+def load_diff_images(path_to_results):
+    image_dirs = glob(path_to_results+'/*')
+    images = [glob(d+'/*diff_6f*')[0] for d in image_dirs] # N x 64 x 64 x 6 array
+    num_images = len(images)
+    # Each image is stored a 64 x 64 x 6 numpy array
+    # Create an empy array to hold all the numpy arrays
+    dataset = np.zeros((num_images, 64, 64, 6), dtype=np.float32)
+    # Store the images in the dataset array
+    for i, img_diff in enumerate(images):
+        dataset[i] = np.load(img_diff).astype(np.float32)
+    print dataset.shape
+    np.random.shuffle(dataset)
+    return dataset
+
+# def load_test_rgb():
+#     test_dir = '/home/hannah/data/mcam_vae/cdf_test'
+#     num_images = len(glob(test_dir + '/*'))
+#     # Create an empty array to hold all the downsampled images
+#     dataset = np.zeros((num_images, 144, 160, 3))
+#     for i, img_fn in enumerate(glob(test_dir + '/*')):
+#         image = cv2.imread(img_fn, cv2.IMREAD_COLOR)
+#         # im_red = image[:,:,2]
+#         # im_blue = image[:,:,0]
+#         # image[:,:,0] = im_red
+#         # image[:,:,2] = im_blue
+#         # dataset[i,:,:,:] = image
+#         dataset[i] = image
+#         #dataset[i] = image.flatten()
+#     print dataset.shape
+#     np.random.shuffle(dataset)
+#     return dataset
